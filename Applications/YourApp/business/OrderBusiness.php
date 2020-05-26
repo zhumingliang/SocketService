1cf478ad90781902c856f3c0fcbdd1d888988a7a
zhumingliang <353575156@qq.com>
2020-05-26 09:56:10 +0800

    测试备餐和取餐刷卡

--- Applications/YourApp/business/OrderBusiness.php
+++ Applications/YourApp/business/OrderBusiness.php
@@ -12,60 +12,69 @@ class OrderBusiness
 
     public function orderStatusHandel($db, $orderId, $code, $codeType)
     {
-        $db->insert('canteen_consumption_log_t')->cols(
-            array(
-                'create_time' => date('Y-m-d H:i:s'),
-                'update_time' => date('Y-m-d H:i:s'),
-                'content' => 1,
-            )
-        )->query();
+        try {
 
-        $errMsg = [
-            'take' => '取餐失败，二维码不正确',
-            'ready' => '备餐失败，二维码不正确'
-        ];
-        $order = $db->select('id,ready_code,take_code,take,ready')->
-        from('canteen_order_t')->where('id= :orderId')
-            ->bindValues(array('orderId' => $orderId))->row();
 
-        $db->insert('canteen_consumption_log_t')->cols(
-            array(
-                'create_time' => date('Y-m-d H:i:s'),
-                'update_time' => date('Y-m-d H:i:s'),
-                'content' => json_encode($order),
-            )
-        )->query();
-        if (empty($order)) {
-            return [
-                'errorCode' => 12000,
-                'msg' => "订单不存在"
-            ];
-        }
-        if ($order["$codeType" . '_code'] != $code) {
-            return [
-                'errorCode' => 12001,
-                'msg' => $errMsg[$codeType]
-            ];
-        }
-        if ($order[$codeType] == 1) {
-            return [
-                'errorCode' => 12002,
-                'msg' => "状态已经修改，无需重复操作"
-            ];
-        }
+            $db->insert('canteen_consumption_log_t')->cols(
+                array(
+                    'create_time' => date('Y-m-d H:i:s'),
+                    'update_time' => date('Y-m-d H:i:s'),
+                    'content' => 1,
+                )
+            )->query();
 
-        $row_count = $db->update('canteen_order_t')->cols(array($codeType))
-            ->where('id=', $orderId)
-            ->bindValue($codeType, 1)->query();
-        if (!$row_count) {
+            $errMsg = [
+                'take' => '取餐失败，二维码不正确',
+                'ready' => '备餐失败，二维码不正确'
+            ];
+            $order = $db->select('id,ready_code,take_code,take,ready')->
+            from('canteen_order_t')->where('id= :orderId')
+                ->bindValues(array('orderId' => $orderId))->row();
+
+            $db->insert('canteen_consumption_log_t')->cols(
+                array(
+                    'create_time' => date('Y-m-d H:i:s'),
+                    'update_time' => date('Y-m-d H:i:s'),
+                    'content' => json_encode($order),
+                )
+            )->query();
+            if (empty($order)) {
+                return [
+                    'errorCode' => 12000,
+                    'msg' => "订单不存在"
+                ];
+            }
+            if ($order["$codeType" . '_code'] != $code) {
+                return [
+                    'errorCode' => 12001,
+                    'msg' => $errMsg[$codeType]
+                ];
+            }
+            if ($order[$codeType] == 1) {
+                return [
+                    'errorCode' => 12002,
+                    'msg' => "状态已经修改，无需重复操作"
+                ];
+            }
+
+            $row_count = $db->update('canteen_order_t')->cols(array($codeType))
+                ->where('id=', $orderId)
+                ->bindValue($codeType, 1)->query();
+            if (!$row_count) {
+                return [
+                    'errorCode' => 12003,
+                    'msg' => "更新状态失败"
+                ];
+            }
             return [
-                'errorCode' => 12003,
-                'msg' => "更新状态失败"
+                'errorCode' => 0,
+                'msg' => "success"
+            ];
+        } catch (\Exception $e) {
+            return [
+                'errorCode' => 12004,
+                'msg' => $e->getMessage()
             ];
         }
-        return [
-            'errorCode' => 0,
-            'msg' => "success"
-        ];
     }
 }
