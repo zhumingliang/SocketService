9601be031244914a95aabdabad68414cbeaa610a
zhumingliang <353575156@qq.com>
2020-10-14 23:54:00 +0800

    ceshi

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -199,12 +199,12 @@ class Events
     {
 
         if ($face == 1) {
-            $sql = "call canteenFaceConsumption(%s,%s,'%s', @currentSubOrderID,@currentParentOrderID,@currentConsumptionType,@resCode,@resMessage,@returnBalance,@returnDinner,@returnDepartment,@returnUsername,@returnPrice,@returnMoney,@returnCount,@returnStrategyType)";
+            $sql = "call canteenFaceConsumption(%s,%s,'%s',%s, @currentSubOrderID,@currentParentOrderID,@currentConsumptionType,@resCode,@resMessage,@returnBalance,@returnDinner,@returnDepartment,@returnUsername,@returnPrice,@returnMoney,@returnCount,@returnStrategyType)";
 
         } else {
-            $sql = "call canteenConsumption(%s,%s,'%s',%s,@currentSubOrderID,@currentParentOrderID,@currentConsumptionType,@resCode,@resMessage,@returnBalance,@returnDinner,@returnDepartment,@returnUsername,@returnPrice,@returnMoney,@returnCount,@returnStrategyType)";
+            $sql = "call canteenConsumption(%s,%s,'%s',%s,%s,@currentSubOrderID,@currentParentOrderID,@currentConsumptionType,@resCode,@resMessage,@returnBalance,@returnDinner,@returnDepartment,@returnUsername,@returnPrice,@returnMoney,@returnCount,@returnStrategyType)";
         }
-        $sql = sprintf($sql, $company_id, $canteen_id, $code, $ic);
+        $sql = sprintf($sql, $company_id, $canteen_id, $code, $ic,time());
         $sql2 = "select @currentSubOrderID,@currentParentOrderID,@currentConsumptionType,@resCode,@resMessage,@returnBalance,@returnDinner,@returnDepartment,@returnUsername,@returnPrice,@returnMoney,@returnCount,@returnStrategyType";
         self::$db->query($sql);
         self::saveLog($sql);
